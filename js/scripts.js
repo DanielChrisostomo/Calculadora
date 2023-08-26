cde3bd9dfc47fc411ad9de65c36a89c17136156b
DanielChrisostomo <daniluz1515@gmail.com>
2023-08-26 17:19:09 -0300

    html, css e seleção de elementos js

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator {
+  constructor(previousOperationText, currentOperationText) {
+    this.previousOperationText = previousOperationText;
+    this.currentOperationText = currentOperationText;
+    this.currentOperation = "";
+  }
+}
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const value = e.target.innerText;
+
+    if (+value >= 0 || value === ".") {
+      console.log(value);
+    } else {
+      console.log("Op: " + value);
+    }
+  });
+});
+
+// https://www.youtube.com/watch?v=KCfaPZ2t2yA
