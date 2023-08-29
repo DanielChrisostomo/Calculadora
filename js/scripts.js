17016fb0e07d0b096f9b552b672ce2585da401e2
DanielChrisostomo <daniluz1515@gmail.com>
2023-08-29 13:44:34 -0300

    divisão adicionada

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,10 @@ class Calculator {
         operationValue = previous - current;
         this.updateScreen(operationValue, operation, current, previous);
         break;
+      case "/":
+        operationValue = previous / current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
       case "*":
         operationValue = previous * current;
         this.updateScreen(operationValue, operation, current, previous);
